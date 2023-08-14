46ffd6b1b0280aad08053d937f46b94b8e860ed5
Luke Waldron <lukewaldron87@gmail.com>
2023-08-14 15:37:15 +0200

    receive messages from RabbitMQ

--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/config/RabbitConfig.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/config/RabbitConfig.java
@@ -1,14 +1,14 @@
 package guru.springframework.rabbit.rabbitstockquoteservice.config;
 
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Delivery;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.rabbitmq.RabbitFlux;
-import reactor.rabbitmq.Sender;
-import reactor.rabbitmq.SenderOptions;
+import reactor.rabbitmq.*;
 
 import javax.annotation.PreDestroy;
 
@@ -33,4 +33,14 @@ public class RabbitConfig {
     Sender sender(Mono<Connection> connectionMono){
         return RabbitFlux.createSender(new SenderOptions().connectionMono(connectionMono));
     }
+
+    @Bean
+    Receiver receiver(Mono<Connection> connectionMono){
+        return RabbitFlux.createReceiver(new ReceiverOptions().connectionMono(connectionMono));
+    }
+
+//    @Bean
+//    Flux<Delivery> deliveryFlux(Receiver receiver){
+//        return receiver.consumeAutoAck(QUEUE);
+//    }
 }
--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteRunner.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteRunner.java
@@ -1,11 +1,18 @@
 package guru.springframework.rabbit.rabbitstockquoteservice.service;
 
+import com.rabbitmq.client.Delivery;
+import guru.springframework.rabbit.rabbitstockquoteservice.config.RabbitConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.rabbitmq.Receiver;
 
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @Component
@@ -14,19 +21,39 @@ public class QuoteRunner implements CommandLineRunner {
 
     private final QuoteGeneratorService quoteGeneratorService;
     private final QuoteMessageSender quoteMessageSender;
+    private final Receiver receiver;
+    //private final Flux<Delivery> deliveryFlux;
 
     @Override
     public void run(String... args) throws Exception {
+
+        CountDownLatch latch = new CountDownLatch(25);
+
         quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100))
                 .take(25)
                 .log("Got Quote")
                 .flatMap(quoteMessageSender::sendQuoteMessage)
                 .subscribe(result -> {
                     log.debug("Send Message to RabbitMQ");
+                    latch.countDown();
                 },throwable -> {
                     log.error("Got Error", throwable);
                 }, () -> {
                     log.debug("All Done");
+                });
+
+        latch.await(1, TimeUnit.SECONDS);
+
+        AtomicInteger receivedCount = new AtomicInteger();
+
+        receiver.consumeAutoAck(RabbitConfig.QUEUE)
+                .log("Msg Receiver")
+                .subscribe(msg ->{
+                    log.debug("Received Message # {} - {}", receivedCount.incrementAndGet(), new String(msg.getBody()));
+                },throwable -> {
+                    log.error("Error Receiving", throwable);
+                }, () -> {
+                    log.debug("Complete");
                 });
     }
 }
